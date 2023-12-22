283e5cdb9fd704c7edd888084d3c9bdc94ec5693
Megachile <69063635+Megachile@users.noreply.github.com>
2023-12-21 20:37:25 -0600

    tecturnarum re-ID
    Moved Andricus tecturnarum records to q-turbinella-woolly-gall (which will be renamed Feron tetyanae when the db gets synched with GF next)

--- DataFix.R
+++ DataFix.R
@@ -12,21 +12,21 @@ setwd(wd)
 gallphen <- dbConnect(RSQLite::SQLite(), "gallphenReset.sqlite")
 
 # sen <- input[which(input$phenophase=="dormant"&input$doy>20&input$doy<60),]
-query <- paste0("WHERE gall_id = '3720' AND host_id = '334' ")
+query <- paste0("WHERE gall_id = '2379'")
 # query <- paste0("WHERE obs_id IN (", paste(sprintf("'%s'",sen$obs_id), collapse = ","),")")
 query <- paste0("WHERE sourceURL LIKE '%source/396%'")
 query <- paste0("WHERE obs_id = '31270'")
 
 # look up ID codes. 
 # The gf_id is what you see in the URL when you search a species on gallformers. The species_id is what the pheno db uses
-select <- paste0("SELECT species_id from species WHERE gf_id = '3823'")
+select <- paste0("SELECT species_id from species WHERE gf_id = '2445'")
 dbGetQuery(gallphen, select)
 
 #look up observations
 select <- paste0("SELECT * FROM observations ", query)
 data <- dbGetQuery(gallphen, select)
-# update <- paste0("UPDATE observations SET host_id = '334'", query)
-# dbExecute(gallphen, update)
+update <- paste0("UPDATE observations SET gall_id = '2531'", query)
+dbExecute(gallphen, update)
 # delete <- paste0("DELETE FROM observations ", query)
 # dbExecute(gallphen, delete)
 
