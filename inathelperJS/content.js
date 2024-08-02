bd9426fdc98e8ed718b56945bf57bfa6f5be74a7
Megachile <69063635+Megachile@users.noreply.github.com>
2024-08-01 20:44:22 -0500

    Action details to popup
    Added action details back to the confirmation popup

--- inathelperJS/content.js
+++ inathelperJS/content.js
@@ -2124,11 +2124,44 @@ async function applyBulkAction() {
         if (selectedAction) {
             const hasDQIRemoval = selectedAction.actions.some(action => action.type === 'qualityMetric' && action.vote === 'remove');
 
-            let confirmMessage = `Are you sure you want to apply "${selectedAction.name}" to ${selectedObservations.size} observations? Note: iNaturalist policy requires that you have individually inspected every observation before you apply this action.`;
+            let confirmMessage = `Are you sure you want to apply "${selectedAction.name}" to ${selectedObservations.size} observation(s)?\n\n`;
+            confirmMessage += "This action includes:\n";
 
-             if (hasDQIRemoval) {
-                    confirmMessage += " Please note: Removing DQI votes cannot be undone in bulk due to API limitations.";
+            selectedAction.actions.forEach(action => {
+                switch (action.type) {
+                    case 'observationField':
+                        confirmMessage += `- Add observation field: ${action.fieldName} = ${action.fieldValue}\n`;
+                        break;
+                    case 'annotation':
+                        const attribute = Object.entries(controlledTerms).find(([_, value]) => value.id === parseInt(action.annotationField));
+                        const attributeName = attribute ? attribute[0] : 'Unknown';
+                        const valueName = attribute ? Object.entries(attribute[1].values).find(([_, id]) => id === parseInt(action.annotationValue))[0] : 'Unknown';
+                        confirmMessage += `- Add annotation: ${attributeName} = ${valueName}\n`;
+                        break;
+                    case 'addToProject':
+                        confirmMessage += `- Add to project: ${action.projectName}\n`;
+                        break;
+                    case 'addComment':
+                        confirmMessage += `- Add comment: "${action.commentBody.substring(0, 50)}${action.commentBody.length > 50 ? '...' : ''}"\n`;
+                        break;
+                    case 'addTaxonId':
+                        confirmMessage += `- Add taxon ID: ${action.taxonName}\n`;
+                        break;
+                    case 'qualityMetric':
+                        const metricName = getQualityMetricName(action.metric);
+                        confirmMessage += `- Set quality metric: ${metricName} to ${action.vote}\n`;
+                        break;
+                    case 'copyObservationField':
+                        confirmMessage += `- Copy field: ${action.sourceFieldName} to ${action.targetFieldName}\n`;
+                        break;
                 }
+            });
+
+            confirmMessage += "\nNote: iNaturalist policy requires that you have individually inspected every observation before you apply this action.";
+
+            if (hasDQIRemoval) {
+                confirmMessage += "\n\nPlease note: Removing DQI votes cannot be undone in bulk due to API limitations.";
+            }
 
             if (confirm(confirmMessage)) {
                 await executeBulkAction(selectedAction, modal);
@@ -2139,7 +2172,6 @@ async function applyBulkAction() {
         document.body.removeChild(modal);
     };
 }
-
 async function getAvailableActions() {
     const customButtons = await new Promise(resolve => browserAPI.storage.sync.get('customButtons', resolve));
     return (customButtons.customButtons || []).filter(button => !button.configurationDisabled);
